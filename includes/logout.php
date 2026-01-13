1258a50902da9eabf07f000f7df674d377dd274c
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-13 10:31:11 +0100

    feat: inital MVC structure project

--- includes/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once(__DIR__ . "/../Classes/db.php");
-require_once(__DIR__ . "/../Classes/user.php");
-
-$pdo = DB::connect();
-$user = new user($pdo);
-$user->logout();
-header("location: ../index.php?msg=Logout Success");
-exit();
--- includes/vehicles/vehicles_data.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once(__DIR__ . "/../../Classes/db.php");
-require_once(__DIR__ . "/../../Classes/vehicle.php");
-
-$pdo = DB::connect();
-$vehicles = new vehicle($pdo);
-
-$vehicles = $vehicles->getVehicles();
