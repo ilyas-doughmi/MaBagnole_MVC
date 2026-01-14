26ddb69b5a70682765ca1689e7522e3a4f5af004
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-14 22:23:23 +0100

    feat: add VehicleController with index method

---
+++ app/controllers/VehicleController.php
@@ -0,0 +1,11 @@
+<?php
+
+class VehicleController{
+
+    public function index()
+    {
+        require_once 'app/views/layouts/header.php';
+        require_once 'app/views/vehicles/showcase.php';
+    }
+
+}
