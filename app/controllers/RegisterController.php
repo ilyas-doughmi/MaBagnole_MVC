2575a9718022526a5f13f0bcf8797109ac9f1357
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-14 22:23:35 +0100

    feat: implement create method in RegisterController for user registration

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -5,5 +5,25 @@ class RegisterController {
         require_once 'app/views/auth/register.php';
     }
 
+    public function create() {
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            require_once 'app/config/config.php';
+            require_once 'app/core/Database.php';
+            require_once 'app/models/Client.php';
 
+            $database = new Database();
+            $db = $database->connect();
+            $client = new Client($db);
+
+            $client->fullname = $_POST['full_name'];
+            $client->email = $_POST['email'];
+            $client->password = $_POST['password'];
+
+            if($client->register()){
+                header('Location: ../login');
+            } else {
+                header('Location: ../register?error=failed');
+            }
+        }
+    }
 }
