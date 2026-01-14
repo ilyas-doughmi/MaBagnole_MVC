9e1660cbfa366bd0d6868982ce03e0dd5dcbeea7
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-14 22:23:29 +0100

    feat: add authenticate method to LoginController for user login functionality

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -5,5 +5,24 @@ class LoginController {
         require_once 'app/views/auth/login.php';
     }
 
+    public function authenticate() {
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            require_once 'app/config/config.php';
+            require_once 'app/core/Database.php';
+            require_once 'app/models/User.php';
 
+            $database = new Database();
+            $db = $database->connect();
+            $user = new user($db);
+
+            $user->email = $_POST['email'];
+            $user->password = $_POST['password'];
+
+            if($user->login()){
+                header('Location: ../home');
+            } else {
+                header('Location: ../login?error=invalid');
+            }
+        }
+    }
 }
